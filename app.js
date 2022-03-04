c12804ccccd9eaf1fe9ed5d2e0f038a1b840e581
Balkar Singh <balkar8199@gmail.com>
2022-03-04 14:53:23 +0530

    04-03

--- app.js
+++ app.js
@@ -16,46 +16,60 @@ app.use(express.static("public"));
 app.use(bp.urlencoded({
   extended: true
 }));
+app.set('view engine', 'ejs');
 app.listen(process.env.PORT || 3000, function() {
   console.log("server started on port 3000");
 });
 
 app.get("/cn", function(req, res) {
   res.sendFile(__dirname+"/cn.html");
+  // res.render('cn', {IPv4:"",res:""});
 });
 
 app.post("/ipdetail",function(req,res){
-  var out=ipdetails(req.body.ip,req.body.mask);
-  res.send(out);
+  var out=ipdetails(req.body.ip,req.body.mask).toString();
+  var inp={ips:req.body.ip,masks:req.body.mask};
+  // res.send(out);
+  res.render('cn',{IPv4:inp,res:out,code:"inpdetail"});
 })
 app.get('/convert',function(req,res){
   console.log('hi');
 })
 app.post("/convert",function(req,res){
-  var out=enc(req.body.cars,req.body.cars1,req.body.mask);
-  res.send(out);
+  var out=enc(req.body.cars,req.body.cars1,req.body.mask).toString();
+  var inp={cars:req.body.cars,cars1:req.body.cars1,masks:req.body.mask};
+  res.render('cn',{IPv4:inp,res:out,code:"convert"});
 })
 app.post("/compress",function(req,res){
-  
-  res.send(compress(req.body.domain));
+  var out=compress(req.body.domain);
+  var inp=(req.body.domain);
+  // res.send(out);
+  res.render('cn',{IPv4:inp,res:out,code:"compress"});
 })
 app.post("/expand",function(req,res){
-  console.log(req.body.do);
-  res.send(expand(req.body.do));
+  // console.log(req.body.do);
+  var out=expand(req.body.do);
+  var inp=req.body.do;
+  // res.send(out);
+  res.render('cn',{IPv4:inp,res:out,code:"expand"});
 })
 app.post("/hostdetail",function(req,res){
   var out=hostdetails(req.body.ip);
-  res.send(out);
+  var inp=req.body.ip;
+  // res.send(out);
+  res.render('cn',{IPv4:inp,res:out,code:"hostdetail"});
 })
 app.post("/myip",function(req,res){
   var x=getip();
   console.log(x);
-  res.send(x);
+  // res.send(x.toString());
+  res.render({IPv4:"Your Ip",out:x,code:"myip"});
 })
 app.post("/i2i",function(req,res){
-  var out=iptodec(req.body.ip);
+  var out=iptodec(req.body.ip).toString();
   console.log(out);
-  res.send(out.toString());
+  // res.send(out);
+  res.render({IPv4:req.body.ip,res:out,code:"i2i"});
 })
 
 
@@ -66,47 +80,66 @@ app.get("/dsa",function(req,res){
 app.post("/inorder",function(req,res){
   var pre=req.body.pre;
   var post=req.body.post;
-  res.send(inorder(pre,post).toString())
+  var out=inorder(pre,post).toString();
+  var inp={pre,post};
+  // res.send(out)
+  res.render('dsa',{IPv4:inp,res:out,code:"inorder"});
 })
 app.post("/postorder",function(req,res){
   var ino=req.body.in;
   var pre=req.body.pre;
-  res.send(postorder(ino,pre).toString())
+  var inp={ino,pre};
+  var out=postorder(ino,pre).toString();
+  // res.send(out);
+  res.render('dsa',{IPv4:inp,res:out,code:"postorder"});
 })
 app.post("/preorder",function(req,res){
   var ino=req.body.in;
   var post=req.body.post;
-  res.send(preorder(ino,post).toString())
+  var inp={ino,post};
+  var out=preorder(ino,post).toString();
+  // res.send(out);
+  res.render('dsa',{IPv4:inp,res:out,code:"preorder"});
 })
 
 app.post("/intopre",function(req,res){
   var ino=req.body.inp;
-  res.send(infixToPrefix(ino));
+  var out=infixToPrefix(ino);
+  // res.send(out);
+  res.render('dsa',{IPv4:ino,res:out,code:"intopre"});
 })
 app.post("/pretopost",function(req,res){
   var ino=req.body.inp;
-  res.send(preToPost(ino));
+  var out=preToPost(ino);
+  // res.send(out);
+  res.render('dsa',{IPv4:ino,res:out,code:"pretopost"});
 })
 app.post("/intopost",function(req,res){
   var ino=req.body.inp;
-  res.send(infixToPostfix(ino));
+  var out=infixToPostfix(ino);
+  // res.send(out);
+  res.render('dsa',{IPv4:ino,res:out,code:"intopost"});
 })
 
 app.post("/job",function(req,res){
   var ids=req.body.ids,
   profit=req.body.profit,
   dead=req.body.dead;
+  var inp={ids,profit,dead};
   var out=JobScheduling(ids,profit,dead);
-  res.send(out.toString());
+  // res.send(out.toString());
+  res.render('dsa',{IPv4:inp,res:out,code:"job"});
 })
 
 app.post("/knap",function(req,res){
   var knap=req.body.knap,
   weight=req.body.weight,
   value=req.body.value;
+  var inp={knap,weight,value};
   var out=knapSackprob(value,weight,knap);
   console.log(out);
-  res.send(out.toString());
+  // res.send(out.toString());
+  res.render('dsa',{IPv4:inp,res:out,code:"knap"});
 })
 
 
@@ -114,7 +147,12 @@ app.post("/activity",function(req,res){
   var name=req.body.name,
   start=req.body.start,
   end=req.body.end;
+  var inp={name:name,start:start,end:end };
   var out=activities(start,end);
-  console.log(out);
-  res.send(out);
+  var o=[];
+  out.forEach(pair =>
+    o.push("("+pair[0]+","+pair[1]+")"));
+  console.log(o);
+  // res.send(o);
+  res.render('dsa',{IPv4:inp,res:o,code:"activity"});
 })
